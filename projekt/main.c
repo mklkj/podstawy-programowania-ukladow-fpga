4fc89736a328830df428592c6fa1b23eef1683bc
Mikołaj Pich <m.pich@outlook.com>
2022-05-28 13:08:56 +0200

    Adjust implementation to our needs

--- projekt/main.c
+++ projekt/main.c
@@ -1,84 +1,120 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cert-msc50-cpp"
-#pragma ide diagnostic ignored "cert-msc50-cpp"
 #pragma ide diagnostic ignored "cert-msc51-cpp"
 
 #include "stdio.h"
 #include "time.h"
 #include "stdlib.h"
+#include "stdbool.h"
+#include "math.h"
 
-const int n = 4; // 10, 20
-const float min = -100; // 10^6
-const float max = +100; // 10^6
+const int N = 4; // 10, 20
+const float MIN = -100; // -10^6
+const float MAX = +100; // +10^6
+const double EPS = 0.0000000001; // dokładność porównania z zerem
 
-void generate_matrix_values(float A[n][n], float x[n], float B[n]);
+void generate_matrix_values(double AB[N][N + 1], double x[N], int WK[N]);
 
-void print_result(float A[n][n], float x[n], float B[n]);
+void solve(double AB[N][N + 1], double x_solved[N], int WK[N]);
 
-void solve(float A[n][n], float B[n], float x_solved[n]);
-
-void check_result(float x_original[n], float x_solved[n]);
+void check_result(const double x_original[N], const double x_solved[N]);
 
 int main() {
     srand(time(NULL));
 
-    float A[n][n];
-    float x[n];
-    float B[n];
+    double AB[N][N + 1];
+    double x[N];
+    int WK[N + 1];
 
-    float x_solved[n];
+    double x_solved[N];
 
-    generate_matrix_values(A, x, B);
-    solve(A, B, x_solved);
+    generate_matrix_values(AB, x, WK);
+    solve(AB, x_solved, WK);
     check_result(x, x_solved);
-    print_result(A, x, B);
 
     return 0;
 }
 
-void generate_matrix_values(float A[n][n], float x[n], float B[n]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            A[i][j] = min + (float) rand() / RAND_MAX * (max - min);
+void generate_matrix_values(double AB[N][N + 1], double x[N], int WK[N]) {
+    // AB
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            AB[i][j] = MIN + (float) rand() / (float) RAND_MAX * (MAX - MIN);
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        x[i] = min + (float) rand() / RAND_MAX * (max - min);
+    // WK
+    for (int i = 0; i < N + 1; ++i) {
+        WK[i] = i;
     }
 
-    float suma = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            suma += A[i][j] * x[j];
+    // x
+    for (int i = 0; i < N; i++) {
+        x[i] = MIN + (float) rand() / (float) RAND_MAX * (MAX - MIN);
+    }
+
+    // B
+    double B[N];
+    double suma = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            suma += AB[i][j] * x[j];
         }
         B[i] = suma;
         suma = 0;
     }
+
+    // AB
+    for (int i = 0; i < N + 1; ++i) {
+        AB[i][N] = B[i];
+    }
 }
 
-void solve(float A[n][n], float B[n], float x_solved[n]) {
-    // metoda gaussa z wyborem elementu głównego
-    // todo
-}
+bool GCEliminujX(int n, double AB[][N + 1], int WK[]) {
+    int i, j, k, tmp;
+    double m;
 
-void check_result(float x_original[n], float x_solved[n]) {
-    // todo
-}
-
-void print_result(float A[n][n], float x[n], float B[n]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%f | ", A[i][j]);
+    for (i = 0; i < n - 1; i++) {
+        for (k = i, j = i + 1; j < n; j++)
+            if (fabs(AB[i][WK[k]]) < fabs(AB[i][WK[j]])) k = j;
+        tmp = WK[k];
+        WK[k] = WK[i];
+        WK[i] = tmp;
+        if (fabs(AB[i][WK[i]]) < EPS) return false;
+        for (j = i + 1; j < n; j++) {
+            m = -AB[j][WK[i]] / AB[i][WK[i]];
+            for (k = i + 1; k <= n; k++) {
+                AB[j][WK[k]] += m * AB[i][WK[k]];
+            }
         }
-        printf("\n");
     }
-    for (int i = 0; i < n; i++) {
-        printf("| %f | \n", x[i]);
+    return true;
+}
+
+bool GCObliczX(int n, double X[], double AB[][N + 1], const int WK[]) {
+    int i, j;
+    double s;
+
+    for (i = n - 1; i >= 0; i--) {
+        if (fabs(AB[i][WK[i]]) < EPS) return false;
+        s = AB[i][n];
+        for (j = n - 1; j > i; j--) s -= AB[i][WK[j]] * X[WK[j]];
+        X[WK[i]] = s / AB[i][WK[i]];
     }
-    printf("Wynik ---------------------\n");
-    for (int i = 0; i < n; i++) {
-        printf("%.14f | \n", B[i]);
+    return true;
+}
+
+void solve(double AB[N][N + 1], double x_solved[N], int WK[N]) {
+    if (GCEliminujX(N, AB, WK) && GCObliczX(N, x_solved, AB, WK)) {
+        printf("Solving succssed!\n");
+    } else printf("Solving failed\n");
+}
+
+void check_result(const double x_original[N], const double x_solved[N]) {
+    for (int i = 0; i < N; ++i) {
+        if (x_original[i] == x_solved[i]) {
+            printf("Position %d match - o: %f, s: %f\n", i, x_original[i], x_solved[i]);
+        } else printf("Position %d not match! - o: %f, s: %f\n", i, x_original[i], x_solved[i]);
     }
 }
 
