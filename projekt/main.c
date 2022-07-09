6d02955b634796c17d6bea2c3c157e64ff011000
Mikołaj Pich <m.pich@outlook.com>
2022-07-09 18:28:43 +0200

    Prepare code for porting to NIOS

--- projekt/main.c
+++ projekt/main.c
@@ -2,11 +2,18 @@
 #pragma ide diagnostic ignored "cert-msc50-cpp"
 #pragma ide diagnostic ignored "cert-msc51-cpp"
 
-#include "stdio.h"
 #include "time.h"
 #include "stdlib.h"
 #include "stdbool.h"
+
+//#include "system.h"
+//#include "alt_types.h"
 #include "math.h"
+//#include "altera_avalon_uart_regs.h"
+//#include "altera_avalon_pio_regs.h"
+#include "string.h"
+#include <stddef.h>
+#include <stdio.h>
 
 #define N 20
 const float MIN = -1000000;
@@ -18,6 +25,25 @@ void generate_matrix_values(double AB[N][N + 1], double x[N], int WK[N]);
 void solve(double AB[N][N + 1], double x_solved[N], int WK[N]);
 
 void check_result(const double x_original[N], const double x_solved[N]);
+
+void send_char(char c) {
+//    while ((IORD_ALTERA_AVALON_UART_STATUS(UART_BASE) & ALTERA_AVALON_UART_STATUS_TRDY_MSK) == 0);
+//    IOWR_ALTERA_AVALON_UART_TXDATA(UART_BASE, c);
+    printf("%c", c);
+}
+
+void printf_nios(char str[]) {
+    int i;
+    for (i = 0; i < strlen(str); i++) {
+        send_char(str[i]);
+    }
+}
+
+void printf_nios2(char str[], double arg) {
+    char buf[32];
+    sprintf(buf, str, arg);
+    printf_nios(buf);
+}
 
 int main() {
     srand(time(NULL));
@@ -39,15 +65,15 @@ void generate_matrix_values(double AB[N][N + 1], double x[N], int WK[N]) {
     int i, j;
 
     // AB
-    printf("Macierz A\n");
+    printf_nios("Macierz A\r\n");
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             AB[i][j] = MIN + (float) rand() / (float) RAND_MAX * (MAX - MIN);
-            printf("| %5f |", AB[i][j]);
+            printf_nios2("| %5f |", AB[i][j]);
         }
-        printf("\n");
+        printf_nios("\r\n");
     }
-    printf("\n");
+    printf_nios("\r\n");
 
     // WK
     for (i = 0; i < N + 1; ++i) {
@@ -55,15 +81,15 @@ void generate_matrix_values(double AB[N][N + 1], double x[N], int WK[N]) {
     }
 
     // x
-    printf("Wektor X (oryginalny)\n");
+    printf_nios("Wektor X (oryginalny)\r\n");
     for (i = 0; i < N; i++) {
         x[i] = MIN + (float) rand() / (float) RAND_MAX * (MAX - MIN);
-        printf("%5f \n", x[i]);
+        printf_nios2("%5f\r\n", x[i]);
     }
-    printf("\n");
+    printf_nios("\r\n");
 
     // B
-    printf("Wektor B (obliczony)\n");
+    printf_nios("Wektor B (obliczony)\r\n");
     double B[N];
     double suma = 0;
     for (i = 0; i < N; i++) {
@@ -71,10 +97,10 @@ void generate_matrix_values(double AB[N][N + 1], double x[N], int WK[N]) {
             suma += AB[i][j] * x[j];
         }
         B[i] = suma;
-        printf("%5f\n", B[i]);
+        printf_nios2("%5f\r\n", B[i]);
         suma = 0;
     }
-    printf("\n");
+    printf_nios("\r\n");
 
     // AB
     for (i = 0; i < N + 1; ++i) {
@@ -125,15 +151,15 @@ void solve(double AB[N][N + 1], double x_solved[N], int WK[N]) {
 void check_result(const double x_original[N], const double x_solved[N]) {
     int i;
 
-    printf("Wektor X' (obliczony)\n");
+    printf_nios("Wektor X' (obliczony)\r\n");
     for (i = 0; i < N; i++) {
-        printf("%5f \n", x_solved[i]);
+        printf_nios2("%5f \r\n", x_solved[i]);
     }
-    printf("\n");
+    printf_nios("\r\n");
 
-    printf("Porownanie\n");
+    printf_nios("Porownanie\r\n");
     for (i = 0; i < N; ++i) {
-        printf("%3e\n", x_original[i] - x_solved[i]);
+        printf_nios2("%3e\r\n", x_original[i] - x_solved[i]);
     }
 }
 
